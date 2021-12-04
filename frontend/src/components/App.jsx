40d9182302d24f2a507e0513fc7e4f31f5c2651e
Rob <iamrobertjeffrey@gmail.com>
2021-12-04 19:30:01 +0000

    made client connect to https

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -46,7 +46,7 @@ const theme = createTheme({
 
 
 })
-const client = io("http://roberts-chatting.herokuapp.com:3002");
+const client = io("https://roberts-chatting.herokuapp.com:3002");
 
 function App()
 {
